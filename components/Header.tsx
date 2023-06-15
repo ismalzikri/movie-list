716b2347473e2616e24cf4e1c41e569653d1296a
Laravel007 <zikriismal42@gmail.com>
2023-06-15 23:27:55 +0700

    update-stuff

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const Header = ({ setQuery }: Props) => {
   return (
-    <div className="sticky flex top-0 z-40 w-full h-24 bg-zinc-900">
+    <div className="sticky flex top-0 z-40 w-full h-20 bg-[rgba(3,37,65,1)]">
       <div className="flex justify-between w-full h-full max-w-7xl m-auto px-4 ">
         <Link href="/">
           <div className="flex items-center cursor-pointer">
@@ -17,7 +17,7 @@ const Header = ({ setQuery }: Props) => {
               <Image
                 width="150"
                 height="50"
-                src="/rmdb-logo.svg"
+                src="/large-logo.svg"
                 alt="rmdb-logo"
               />
             </div>
@@ -25,7 +25,7 @@ const Header = ({ setQuery }: Props) => {
               <Image
                 width="42"
                 height="42"
-                src="/rmdb-logo-small.svg"
+                src="/tmdb-logo.svg"
                 alt="rmdb-logo-small"
               />
             </div>
